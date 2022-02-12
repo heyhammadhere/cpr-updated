0cdd0b273b7353fd0e47b4f6e2de3eb0e9aa8231
Muhammad Hammad <heyhammadhere@gmail.com>
2022-02-12 19:25:01 +0500

    v0.1.2

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,59 +1,55 @@
-
-$('document').ready(function() {
-  $('#doctorSlideshow').owlCarousel({
+$("document").ready(function () {
+  $("#doctorSlideshow").owlCarousel({
     nav: true,
     dots: false,
-    navText: ["<span class='mai-arrow-back'></span>", "<span class='mai-arrow-forward'></span>"],
+    navText: [
+      "<span class='mai-arrow-back'></span>",
+      "<span class='mai-arrow-forward'></span>",
+    ],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       576: {
-        items: 2
+        items: 2,
       },
       992: {
-        items: 3
-      }
-    }
+        items: 3,
+      },
+    },
   });
 });
 
-$('document').ready(function() {
-  $("a[data-role='smoothscroll']").click(function(e) {
+$("document").ready(function () {
+  $("a[data-role='smoothscroll']").click(function (e) {
     e.preventDefault();
-    
+
     var position = $($(this).attr("href")).offset().top - nav_height;
 
-    $("body, html").animate({
-        scrollTop: position
-    }, 1000 );
+    $("body, html").animate(
+      {
+        scrollTop: position,
+      },
+      1000
+    );
     return false;
   });
 });
 
-$('document').ready(function() {
+$("document").ready(function () {
   // Back to top
   var backTop = $(".back-to-top");
 
-  $(window).scroll(function() {
-    if($(document).scrollTop() > 400) {
-      backTop.css('visibility', 'visible');
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 400) {
+      backTop.css("visibility", "visible");
+    } else if ($(document).scrollTop() < 400) {
+      backTop.css("visibility", "hidden");
     }
-    else if($(document).scrollTop() < 400) {
-      backTop.css('visibility', 'hidden');
-    }
-  });
-
-  backTop.click(function() {
-    $('html').animate({
-      scrollTop: 0
-    }, 1000);
-    return false;
   });
 });
 
-
-$('document').ready(function() {
+$("document").ready(function () {
   // Tooltips
   $('[data-toggle="tooltip"]').tooltip();
 
@@ -63,4 +59,3 @@ $('document').ready(function() {
   // Page scroll animate
   new WOW().init();
 });
-
